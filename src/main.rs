5f39636139dc64284e6e40055495a22af52c9225
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-01-05 15:17:58 +0900

    SMFファイルのパスをを引数で受け取る

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,109 @@
+use clap::Parser;
 use midly::num::u7;
-use std::fs::{create_dir_all, File};
+use std::fs::{create_dir, File};
 use std::io::{BufWriter, Write};
-use std::path::Path;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input standard MIDI file to convert
+    input: std::path::PathBuf,
+}
 
 struct Note {
     midi_note_number: Option<u7>,
     start_position: u32,
 }
 
-fn main() {
-    let output_dir = Path::new("out");
-    create_dir_all(&output_dir).unwrap();
-    let mut w = BufWriter::new(File::create(output_dir.join("test.csv")).unwrap());
-    writeln!(w, "NoteNumber,StartPosition,Length").unwrap();
+/// 出力先フォルダを作成
+fn create_export_folder(path: &PathBuf) -> PathBuf {
+    let mut export_path = PathBuf::from(path.clone());
+    export_path.pop();
+    export_path.push("export");
+    let metadata = path.clone().metadata().unwrap();
+    assert_eq!(false, metadata.permissions().readonly());
 
-    let data = std::fs::read("test.mid").unwrap();
-    let smf = midly::Smf::parse(&data).unwrap();
+    if !export_path.exists() {
+        match create_dir(export_path.as_path()) {
+            Err(e) => eprintln!("Failed on create \"export\" dir : {}", e),
+            Ok(_) => {}
+        }
+    }
+    export_path
+}
+
+/// Pathがwavファイルかどうか
+fn is_midi_file(path: &PathBuf) {
+    match path.extension() {
+        Some(x) => {
+            if x.is_empty() {
+                eprintln!("Input file extension is must to be \"mid\" or \"smf\"");
+                std::process::exit(1);
+            }
+
+            match x.to_str().unwrap() {
+                "mid" => {}
+                "MID" => {}
+                "smf" => {}
+                "SMF" => {}
+                _ => {
+                    eprintln!("Input file extension is must to be \"wav\"");
+                    std::process::exit(1);
+                }
+            }
+        }
+        None => {}
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let input = args.input.clone();
+    if !input.exists() {
+        eprintln!("{} is not found", input.display());
+        std::process::exit(1);
+    }
+
+    is_midi_file(&input); //inputがMIDIファイルか確認
+    let input = input.canonicalize().unwrap(); //絶対パスに変換
+    let input_filename = args.input.clone();
+    let input_filename = input_filename.file_stem().unwrap().to_str().unwrap();
+
+    // exportフォルダを作成
+    let mut export_path = create_export_folder(&input);
+    println!(
+        "Export folder: {}",
+        export_path.canonicalize().unwrap().display()
+    );
+    export_path.push(format!("{}.csv", input_filename)); //変換後のCSVファイル
+
+    // SMFファイルのHeader情報などをprint
+    let input = std::fs::read(input).unwrap();
+    let smf = midly::Smf::parse(&input).unwrap();
+    println!("SMF info");
+    println!("MIDI file has {} tracks", smf.tracks.len());
+    println!("Timing: {:?}", smf.header.timing);
+
     let mut note = Note {
         midi_note_number: None,
         start_position: 0,
     };
 
-    println!("midi file has {} tracks!", smf.tracks.len());
-    println!("tempo: {:?}", smf.header.timing);
+    let mut w = BufWriter::new(File::create(export_path).unwrap());
+    writeln!(w, "NoteNumber,StartPosition,Length").unwrap();
 
     for tr in smf.tracks {
         let mut current_position: u32 = 0;
         for e in tr {
             current_position += e.delta.as_int();
-            println!("{:?}, {}", e, current_position);
             if let midly::TrackEventKind::Midi { channel, message } = e.kind {
                 match message {
                     //ノートオン
                     midly::MidiMessage::NoteOn { key, vel } => {
                         if let Some(n) = note.midi_note_number {
                             let length = current_position - note.start_position;
-                            writeln!(
-                                w,
-                                "{},{},{}",
-                                note.midi_note_number.unwrap(),
-                                note.start_position,
-                                length
-                            )
-                            .unwrap();
+                            writeln!(w, "{},{},{}", n, note.start_position, length).unwrap();
                             note.midi_note_number = None;
                         }
 
